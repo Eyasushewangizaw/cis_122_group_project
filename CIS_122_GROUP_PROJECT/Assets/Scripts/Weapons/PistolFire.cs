7e9b4f59666f509f40b27fdf098b9f114e8f6440
Eyasushewangizaw <112728084+Eyasushewangizaw@users.noreply.github.com>
2022-10-29 16:17:17 -0400

    script shoot

--- CIS_122_GROUP_PROJECT/Assets/Scripts/Weapons/PistolFire.cs
+++ CIS_122_GROUP_PROJECT/Assets/Scripts/Weapons/PistolFire.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PistolFire : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject blackPistol;
+    public bool isFiring = false;
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isFiring == false)
+            {
+                StartCoroutine(FireThePistol());
+            }
+        }
         
+    }
+    IEnumerator FireThePistol()
+    {
+        isFiring = true;
+        blackPistol.GetComponent<Animator>().Play("FirePistol");
+        yield return new WaitForSeconds(0.25f);
+        blackPistol.GetComponent<Animator>().Play("New State");
+        isFiring=false;
     }
 }
