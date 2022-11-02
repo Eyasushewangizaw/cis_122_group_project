664c8e6750b772d25a08e9b014476316402b3999
Eyasushewangizaw <112728084+Eyasushewangizaw@users.noreply.github.com>
2022-11-02 19:59:51 -0400

    .

--- CIS_122_GROUP_PROJECT/Assets/Scripts/Weapons/PistolFire.cs
+++ CIS_122_GROUP_PROJECT/Assets/Scripts/Weapons/PistolFire.cs
@@ -7,7 +7,7 @@ public class PistolFire : MonoBehaviour
     public GameObject blackPistol;
     public bool isFiring = false;
     public GameObject muzzleflash;
-
+    public AudioSource pistolShot;
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
@@ -23,6 +23,7 @@ public class PistolFire : MonoBehaviour
     {
         isFiring = true;
         blackPistol.GetComponent<Animator>().Play("FirePistol");
+        pistolShot.Play();
         muzzleflash.SetActive(true);
         yield return new WaitForSeconds(0.05f);
         muzzleflash.SetActive(false);
